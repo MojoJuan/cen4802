695dc05f5fbb0f98ebd9427b7b834457c01dbdf8
MojoJuan <123435500+MojoJuan@users.noreply.github.com>
2023-09-05 18:19:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main
+{
+    //method to return the n-th fibonacci sequence number
+    public static int fibonacci(int n)
+    {
+    //if n is 1 or less then return n
+    if (n <= 1)
+       return n;
+    //recursively call the function fibonacci()
+    return fibonacci(n-1) + fibonacci(n-2);
+    }
+    //main() method 
+    public static void main (String args[])
+    {
+    //delare one variable n
+    int n;
+    //create Scanner class Object
+    Scanner scan=new Scanner(System.in);
+    //get value of n from user
+    System.out.println("Enter value of n: ");
+    n = scan.nextInt();
+    //print n-th number in sequence
+    System.out.println("The " + n + "th term of the Fibonacci sequence is " + fibonacci(n));
+
+      scan.close();
+    }
+}
